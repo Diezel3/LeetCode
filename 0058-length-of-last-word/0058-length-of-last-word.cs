7c019a5c1cb59469c11dd81eba3055d2c6e3ba05
Vincent <154533673+Diezel3@users.noreply.github.com>
2024-09-02 14:21:19 +0100

    Time: 44 ms (67.72%), Space: 38.1 MB (81.3%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cs
+++ 0058-length-of-last-word/0058-length-of-last-word.cs
@@ -1,18 +1,12 @@
 public class Solution {
     public int LengthOfLastWord(string s) {
         int count = 0;
+        s = s.TrimEnd();
         for (int i = s.Length - 1; i >= 0; i--)
         {
             if(s[i] == ' ')
             {
-                if(count > 0)
-                {
-                    break;
-                }
-                else
-                {
-                    continue;
-                }
+                break;
             }
             else
             {
