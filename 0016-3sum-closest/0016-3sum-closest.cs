b0d83de3af20ab29b67467ef36a9cacd0625092c
Vincent <154533673+Diezel3@users.noreply.github.com>
2024-09-15 01:23:18 +0100

    Update 0016-3sum-closest.cs (Optimisation comments)
    Added comments for later optimisation of the code to use the Two pointer technique which reduces the time complexity of my code.

--- 0016-3sum-closest/0016-3sum-closest.cs
+++ 0016-3sum-closest/0016-3sum-closest.cs
@@ -1,5 +1,13 @@
 public class Solution {
     public int ThreeSumClosest(int[] nums, int target) {
+        // OPTIMISE LATER USING THE TWO POINTER TECHNIQUE
+        // SORT THE ARRAY FIRST IN ASCENDING ORDER
+        // USE FOR LOOP AND INITIALISE firstNum = [i]
+        // INITIALISE leftPointer = [I + 1]
+        // INITIALISE rightPointer = [nums.Length - 1]
+        // WHILE ITERATING, IF GREATER THAN TARGET, REDUCE rightPointer
+        // IF LESS THAN TARGET, INCREASE leftPointer
+        // THEN STORE SUM AND COMPARE WITH PREVIOUS SUM FOR CLOSEST TO TARGET
        int total = 0;
        int newDifference;
        int oldDifference = 0;
