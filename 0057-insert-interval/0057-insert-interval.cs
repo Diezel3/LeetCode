dabe18bd0a6c237e2ce4e165f0b338442625c0c6
Vincent <154533673+Diezel3@users.noreply.github.com>
2024-10-14 20:24:59 +0100

    Time: 252 ms (5.52%), Space: 50.6 MB (14.21%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cs
@@ -0,0 +1,39 @@
+public class Solution {
+    public int[][] Insert(int[][] intervals, int[] newInterval) {
+        List<int[]> intervalsList = new List<int[]>(intervals);
+        intervalsList.Add(newInterval);
+        intervalsList.Sort((a, b) => a[0].CompareTo(b[0]));
+        intervalsList.Sort((a, b) => a[1].CompareTo(b[1]));
+        bool sorting = true;
+        int count = 0;
+        while (sorting == true)
+        {
+            for (int i = 0; i < intervalsList.Count - 1; i++)
+            {
+                if (intervalsList[i+1][0] <= intervalsList[i][1])
+                {
+                    if (intervalsList[i+1][0] < intervalsList[i][0])
+                    {
+                        intervalsList[i][0] = intervalsList[i+1][0];
+                    }
+                    if (intervalsList[i+1][1] > intervalsList[i][1])
+                    {
+                        intervalsList[i][1] = intervalsList[i+1][1];
+                    }
+                    //intervalsList[i][1] = intervalsList[i+1][1];
+                    intervalsList.RemoveAt(i+1);
+                    i--;
+                    count++;
+                }
+            }
+            if(count == 0)
+            {
+                break;
+            }
+            count = 0;
+        }
+      
+        intervals = intervalsList.ToArray();
+        return intervals;
+    }
+}
